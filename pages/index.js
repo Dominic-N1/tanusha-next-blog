5095575d2163e7f1a5f1012a20b22d0a2fcfce7d
Dominic-N1 <vladimirndsm@gmail.com>
2022-10-20 02:08:54 +0700

    date & sort

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,26 @@
 import fs from "fs";
 import path from "path";
+import Link from "next/link";
 import matter from "gray-matter";
 import Layout from "../components/Layout";
+import Post from "../components/Post";
+import { sortByDate } from "../utils/sort";
+import dateFormatter from "../utils/dateFormatter";
 
 export default function HomePage({ posts }) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1 className="text-5xl border-b-4 p-5 font-bold">Latest Posts</h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {posts.map((post, index) => (
+          <Post key={index} post={post} />
+        ))}
+      </div>
+      <Link href="/blog">
+        <a className="block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full">
+          All Posts
+        </a>
+      </Link>
     </Layout>
   );
 }
@@ -22,7 +36,11 @@ export async function getStaticProps() {
       "utf-8"
     );
 
-    const { data: frontmatter } = matter(markdownWithMeta);
+    const { data: data } = matter(markdownWithMeta);
+    const frontmatter = {
+      ...data,
+      date: dateFormatter.format(new Date(data.date)),
+    };
 
     return {
       slug,
@@ -32,7 +50,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      posts,
+      posts: posts.sort(sortByDate).slice(0, 6),
     },
   };
 }
