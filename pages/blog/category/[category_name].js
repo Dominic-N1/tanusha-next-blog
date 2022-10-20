e0647f6d4adaaff369c39a066febb27d9645e011
Dominic-N1 <vladimirndsm@gmail.com>
2022-10-20 13:22:42 +0700

    category list

--- pages/blog/category/[category_name].js
+++ pages/blog/category/[category_name].js
@@ -2,21 +2,30 @@ import fs from "fs";
 import path from "path";
 import Layout from "@/components/Layout";
 import Post from "@/components/Post";
+import CategoryList from "@/components/CategoryList";
 import matter from "gray-matter";
 import { getPosts } from "@/lib/posts";
 import dateFormatter from "@/utils/dateFormatter";
 
-export default function CategoryBlogPage({ posts, categoryName }) {
+export default function CategoryBlogPage({ posts, categoryName, categories }) {
   return (
     <Layout>
-      <h1 className="text-5xl border-b-4 p-5 font-bold">
-        Posts in {categoryName}
-      </h1>
+      <div className="flex justify-between">
+        <div className="w-3/4 mr-10">
+          <h1 className="text-5xl border-b-4 p-5 font-bold">
+            Posts in {categoryName}
+          </h1>
 
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {posts.map((post, index) => (
-          <Post key={index} post={post} />
-        ))}
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+            {posts.map((post, index) => (
+              <Post key={index} post={post} />
+            ))}
+          </div>
+        </div>
+
+        <div className="w-1/4">
+          <CategoryList categories={categories} />
+        </div>
       </div>
     </Layout>
   );
@@ -55,6 +64,9 @@ export async function getStaticProps({ params: { category_name } }) {
 
   const posts = getPosts();
 
+  const categories = posts.map((post) => post.frontmatter.category);
+  const uniqueCategories = [...new Set(categories)];
+
   const categoryPosts = posts.filter(
     (post) => post.frontmatter.category.toLowerCase() === category_name
   );
@@ -63,6 +75,7 @@ export async function getStaticProps({ params: { category_name } }) {
     props: {
       posts: categoryPosts,
       categoryName: category_name,
+      categories: uniqueCategories,
     },
   };
 }
