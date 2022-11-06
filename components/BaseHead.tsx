710e1df5ea3676c39d839215060d17bfd797e68f
Dominic-N1 <vladimirndsm@gmail.com>
2022-11-06 19:27:35 +0700

    add image

--- components/BaseHead.tsx
+++ components/BaseHead.tsx
@@ -15,14 +15,15 @@ export default function BaseHead({
   description,
   siteName,
   siteUrl,
-  image = "public/leaf-heart.svg",
+  image = "/leaf-heart.svg",
   children,
 }) {
   return (
     <>
       {/* Global Metadata  */}
       <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />{" "}
+      <meta http-equiv="Content-Language" content="ru_RU" />
       <link
         rel="icon"
         href="/leaf-heart.svg"
@@ -31,25 +32,19 @@ export default function BaseHead({
       />
       <meta name="revisit-after" content="30 days" />
       <meta name="author" content="Domscript" />
-
       {/* <meta name="generator" content={generator} /> */}
-
       <meta name="keywords" content={keywords} />
       <meta name="description" content={description} />
       <meta name="news_keywords" content={keywords} />
-
       <meta name="next-head-count" content="8" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge,Chrome=1" />
-      <meta http-equiv="Content-Language" content="ru_RU" />
       <meta name="referrer" content="origin-when-cross-origin" />
       <meta name="google" content="notranslate" />
       <link rel="canonical" href={siteUrl} />
-
       {/* Primary Meta Tags  */}
       <title>{title}</title>
       <meta name="title" content={title} />
       <meta name="description" content={description} />
-
       {/* Open Graph / Facebook  */}
       <meta property="og:type" content="website" />
       <meta property="og:url" content={siteUrl} />
@@ -57,7 +52,6 @@ export default function BaseHead({
       <meta property="og:description" content={description} />
       <meta property="og:site_name" content={siteName} />
       <meta property="og:image" content={new URL(image, siteUrl).toString()} />
-
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={siteUrl} />
